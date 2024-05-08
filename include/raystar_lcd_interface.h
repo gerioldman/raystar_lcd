9ddebdbbb05682f70478558c9bceca3758aa266c
gerioldman <k.gergo49@gmail.com>
2024-05-09 00:16:13 +0200

    General improvements

--- include/raystar_lcd_interface.h
+++ include/raystar_lcd_interface.h
@@ -1,11 +1,11 @@
 /**
- * @file LCD_HWI.h
- * @brief Defines interface for LCD_Driver to interact with hardware dependant calls
- * @date 2022-11-21-15:40:17
-*/
+ * @file raystar_lcd_interface.h
+ * @author gerioldman (k.gergo49@gmail.com)
+ * @brief Defines interface for the driver to interact with hardware dependant calls
+ */
 
-#ifndef LCD_DRIVER_INTERFACE_H
-#define LCD_DRIVER_INTERFACE_H
+#ifndef RAYSTAR_LCD_INTEFACE_H
+#define RAYSTAR_LCD_INTEFACE_H
 
 #include <stdint.h>
 
@@ -13,47 +13,47 @@
 
 /**
  * @brief Enumeration for the LCD_HWI function call return values
- * 
+ *
  */
 typedef enum
 {
-    LCD_HWI_OK,
-    LCD_HWI_NOT_OK
-}LCD_HWI_Status;
+	LCD_HWI_OK,
+	LCD_HWI_NOT_OK
+} LCD_HWI_Status;
 
 /**
  * @brief Enumeration for the LCD Enable pin's state
- * 
+ *
  */
 typedef enum
 {
 	LCD_HWI_Enable_OFF = 0,
 	LCD_HWI_Enable_ON = 1
-}LCD_HWI_Enable_State;
+} LCD_HWI_Enable_State;
 
 /**
  * @brief Enumeration for the LCD Latch pin's state
- * 
+ *
  */
 typedef enum
 {
 	LCD_HWI_Latch_OFF = 0,
 	LCD_HWI_Latch_ON = 1
-}LCD_HWI_Latch_State;
+} LCD_HWI_Latch_State;
 
 /**
  * @brief Enumeration for the LCD Data/Instruction pin's state
- * 
+ *
  */
 typedef enum
 {
 	LCD_HWI_DI_Data,
 	LCD_HWI_DI_Instruction
-}LCD_HWI_Instruction_Data_State;
+} LCD_HWI_Instruction_Data_State;
 
 /**
  * @brief Enumeration for the LCD Chip select lines state
- * 
+ *
  */
 typedef enum
 {
@@ -61,31 +61,31 @@ typedef enum
 	LCD_HWI_CS1 = 1,
 	LCD_HWI_CS2 = 2,
 	LCD_HWI_CS12 = 3
-}LCD_HWI_CS_Lines_State;
+} LCD_HWI_CS_Lines_State;
 
 /**
  * @brief Enumeration for the LCD Read/Write pin's state
- * 
+ *
  */
 typedef enum
 {
 	LCD_HWI_RW_Write,
 	LCD_HWI_RW_Read
-}LCD_HWI_RW_State;
+} LCD_HWI_RW_State;
 
 /**
  * @brief Enumeration for the LCD RESET pin's state
- * 
+ *
  */
 typedef enum
 {
 	LCD_HWI_RST_ON,
 	LCD_HWI_RST_OFF
-}LCD_HWI_RST_State;
+} LCD_HWI_RST_State;
 
 /**
  * @brief Provides call to set the R/W pins state
- * 
+ *
  * @param state
  * @return LCD_HWI_Status, returns success of the function call
  */
@@ -93,7 +93,7 @@ LCD_HWI_Status LCD_HWI_SetRWLine(LCD_HWI_RW_State state);
 
 /**
  * @brief Provides call to set the LCD datalines
- * 
+ *
  * @param value takes a byte long value, and sets the 8 lines value to the corresponding bits value
  * @return LCD_HWI_Status, returns success of the function call
  */
@@ -101,24 +101,24 @@ LCD_HWI_Status LCD_HWI_SetDataLines(uint8_t value);
 
 /**
  * @brief Provides call to set the LCD Chip select lines
- * 
- * @param state 
+ *
+ * @param state
  * @return LCD_HWI_Status, returns success of the function call
  */
 LCD_HWI_Status LCD_HWI_SetCSLines(LCD_HWI_CS_Lines_State state);
 
 /**
  * @brief Provides call to set the state of the Data/Instruction pin
- * 
- * @param state 
+ *
+ * @param state
  * @return LCD_HWI_Status, returns success of the function call
  */
 LCD_HWI_Status LCD_HWI_SetDILine(LCD_HWI_Instruction_Data_State state);
 
 /**
  * @brief Provides call to set the state of the Enable pin
- * 
- * @param enable 
+ *
+ * @param enable
  * @return LCD_HWI_Status, returns success of the function call
  */
 LCD_HWI_Status LCD_HWI_SetEnable(LCD_HWI_Enable_State enable);
@@ -141,9 +141,9 @@ LCD_HWI_Status LCD_HWI_SetReset(LCD_HWI_RST_State state);
 
 /**
  * @brief Provides call to delay execution by given number of microseconds
- * 
- * @param us 
+ *
+ * @param us
  */
-void LCD_HWI_Delay(uint32_t us);			
+void LCD_HWI_Delay(uint32_t us);
 
-#endif // LCD_DRIVER_INTERFACE_H
+#endif // RAYSTAR_LCD_INTEFACE_H
